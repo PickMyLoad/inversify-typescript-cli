0f7f4016639d5524dfe91fbf81bb202b9b7b2fa1
Dan Imbrogno <dan@3vl.ca>
2018-01-17 16:26:11 -0500

    Fixes bug in name of component and modules when in sub folders

--- src/helpers/names.spec.ts
+++ src/helpers/names.spec.ts
@@ -6,6 +6,7 @@ describe('names', () => {
   it('should return correct casing', () => {
 
     expect(names.getModuleVarName('Hi')).to.eql('hi');
+    expect(names.getModuleVarName('Sub/Hi')).to.eql('hi');
     expect(names.getModuleDirName('Hi')).to.eql(['hi']);
     expect(names.getModuleDirName('Sub/Hi')).to.eql(['sub', 'hi']);
     expect(names.getModuleFileName('Hi')).to.eql(['hi', 'hi.ts']);
@@ -13,7 +14,9 @@ describe('names', () => {
     expect(names.getModuleFileName('A/B/Hi')).to.eql(['a', 'b', 'hi', 'hi.ts']);
     expect(names.getComponentFileName('hi')).to.eql(['Hi.ts']);
     expect(names.getComponentClassName('hi')).to.eql('Hi');
+    expect(names.getComponentClassName('Sub/hi')).to.eql('Hi');
     expect(names.getComponentInterfaceName('hi')).to.eql('IHi');
+    expect(names.getComponentInterfaceName('Sub/hi')).to.eql('IHi');
     expect(names.getRefName('hi')).to.eql('Hi');
 
   });
--- src/helpers/names.ts
+++ src/helpers/names.ts
@@ -14,7 +14,7 @@ const formatPathSegments = (segment: string, transformer: (val: string) => strin
 };
 
 export const names = {
-  getModuleVarName: (name: string) => changeCase.camelCase(name),
+  getModuleVarName: (name: string) => changeCase.camelCase(path.basename(name)),
   getModuleDirName: (name: string) => formatPathSegments(name, changeCase.camelCase),
   getModuleFileName: (name: string) => [
     ...formatPathSegments(name, changeCase.camelCase), // get the directory portion
@@ -22,7 +22,7 @@ export const names = {
   ],
   getComponentFileName: (name: string) => formatPathSegments(name, changeCase.pascalCase, '.ts'),
   getComponentTestFileName: (name: string) => formatPathSegments(name, changeCase.pascalCase, '.spec.ts'),
-  getComponentClassName: (name: string) => changeCase.pascalCase(name),
-  getComponentInterfaceName: (name: string) => `I${changeCase.pascalCase(name)}`,
+  getComponentClassName: (name: string) => changeCase.pascalCase(path.basename(name)),
+  getComponentInterfaceName: (name: string) => `I${changeCase.pascalCase(path.basename(name))}`,
   getRefName: (name: string) => changeCase.pascalCase(name)
 };
